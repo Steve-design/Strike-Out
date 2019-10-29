f7e3f8d0b523f88fbf5d927968269766424ce047
IKERRIZ <okothfaith94@gmail.com>
2019-10-29 10:20:34 +0300

    Add an instance of the logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -40,3 +40,9 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
