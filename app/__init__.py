779c63275149829dc06ea1bd92f861c9459e78b5
Steve-design <smzalendo31@gmail.com>
2019-10-29 11:58:24 +0300

    instanciate the app

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,15 @@ from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_sqlalchemy import SQLAlchemy
 from flask_simplemde import SimpleMDE
+
+
+simple = SimpleMDE()
+
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+bootstrap = Bootstrap()
+db = SQLAlchemy()
