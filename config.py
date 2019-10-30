299dd6dac6f059678a40e17f50fde7271f2262f4
Steve-design <smzalendo31@gmail.com>
2019-10-30 10:43:36 +0300

    make main ready

--- config.py
+++ config.py
@@ -42,3 +42,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}  
--- manage.py
+++ manage.py
@@ -21,3 +21,7 @@ def test():
 @manager.shell
 def make_shell_context():
     return dict(app=app, db=db, User=User, Comments=Comments, Article=Article, ArticleCategory=ArticleCategory, Quote=Quote)   
+
+
+if __name__=='__main__':
+    manager.run()    
