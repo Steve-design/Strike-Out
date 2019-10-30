1d4aded45e6d1a91a0c89c139c4a98a669a8dc19
Steve-design <smzalendo31@gmail.com>
2019-10-30 09:34:17 +0300

    update imported modules

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
-rom app import create_app,db
-from app.models import User,Pitch,Comment
-from  flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
+from app.models import User, Comments, Article, UpVote,DownVote, ArticleCategory, Quote
 from flask_script import Manager,Server
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
