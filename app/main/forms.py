a25010382ffb14e6912a1e8e072681af63015077
Steve-design <smzalendo31@gmail.com>
2019-10-30 07:42:48 +0300

    create a class for comment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,7 @@ from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave your comment...',validators = [Required()])
+    submit = SubmitField('Submit')     
