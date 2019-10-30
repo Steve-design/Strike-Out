5e76ec42568cf001ce6a5507e36032409bc2ac27
Steve-design <smzalendo31@gmail.com>
2019-10-30 10:48:28 +0300

    import required modules

--- app/__init__.py
+++ app/__init__.py
@@ -25,11 +25,11 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
-     app.config.from_object(config_options[config_name])
+    app.config.from_object(config_options[config_name])
 
     configure_uploads(app, photos)
 
-     bootstrap.init_app(app)
+    bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
