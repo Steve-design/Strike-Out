d01d43f5fc0a3e539cbe46813e9dc3e5359e0777
Steve-design <smzalendo31@gmail.com>
2019-10-30 08:41:47 +0300

    update imports and create index view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,22 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-from .forms import *
-from ..models import *
 from flask_login import login_required, current_user
-from ..import db,photos
-import markdown2
-from ..email import mail_message
-from ..requests import get_quotes
+from .forms import ReviewForm,CategoryForm,CommentForm,BlogForm
+from ..models import ArticleCategory,Article,Comments,UpVote,DownVote
+# from ..models import Review
+# Review = review.Review
+#display categories on the landing page
+from ..request import get_quote
 
 @main.route('/')
 def index():
-    posts = Post.query.order_by(Post.date_posted.desc()).all()
-    quotes = get_quotes()
-    return render_template('index.html', quotes=quotes, posts=posts)
+    """ View root page function that returns index page """
+
+    category = ArticleCategory.get_categories()
+    quote = get_quote()
+
+    title = 'Home- Welcome'
+    return render_template('index.html', title = title, categories=category, quote=quote)
 
 @main.route('/user/<uname>')
 def profile(uname):
