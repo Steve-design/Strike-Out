5b2a0f4d900c65738f2d04a7703603679b77b840
Steve-design <smzalendo31@gmail.com>
2019-10-30 09:26:09 +0300

    create an instance of app development

--- manage.py
+++ manage.py
@@ -2,3 +2,5 @@ rom app import create_app,db
 from app.models import User,Pitch,Comment
 from  flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
+
+app = create_app('development')
