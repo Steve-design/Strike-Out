321ac370a3b6f613ba2fadd3959f1d6c67c3cdfa
Steve-design <smzalendo31@gmail.com>
2019-10-30 09:51:51 +0300

    add a make shell function

--- manage.py
+++ manage.py
@@ -17,3 +17,7 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Comments=Comments, Article=Article, ArticleCategory=ArticleCategory, Quote=Quote)   
