f81c77408deee2fee4979687e778cc8b1d8c0d10
Steve-design <smzalendo31@gmail.com>
2019-10-30 10:49:53 +0300

    function to load user

--- app/models.py
+++ app/models.py
@@ -4,3 +4,11 @@ from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
 from sqlalchemy.sql import func
+
+@login_manager.user_loader
+def load_user(user_id):
+    '''
+    @login_manager.user_loader Passes in a user_id to this function
+    Function queries the database and gets a user's id as a response
+    '''
+    return User.query.get(int(user_id))
