b1a8a0e1b4aa102e7022affffa800ff478d1f12a
Steve-design <smzalendo31@gmail.com>
2019-10-31 12:12:31 +0300

    create a sender email function

--- app/email.py
+++ app/email.py
@@ -2,3 +2,10 @@ from flask_mail import Message
 from flask import render_template
 from . import main
 
+def mail_message(subject, template, to, **kwargs):
+    sender_email='smzalendo31@gmail.com'
+
+    email = Message(subject,sender=sender_email,recipients=[to])
+    email.body=render_template(template + ".txt", **kwargs)
+    email.html = render_template(template + ".html", **kwargs)
+    mail.send(email)
