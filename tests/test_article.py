4aee213cebe258ab6b137faa1e14732723ed7b6c
IKERRIZ <okothfaith94@gmail.com>
2019-10-31 10:19:26 +0300

    Corrected the indentation

--- tests/test_article.py
+++ tests/test_article.py
@@ -5,17 +5,20 @@ class UserModelTest(unittest.TestCase):
 
     def setUp(self):
        self.new_user = User(password = 'banana')
-   def test_password_setter(self):
+
+    def test_password_setter(self):
        self.assertTrue(self.new_user.pass_secure is not None)
-def test_no_access_password(self):
+
+    def test_no_access_password(self):
            with self.assertRaises(AttributeError):
                self.new_user.password
-def test_password_verification(self):
-   self.assertTrue(self.new_user.verify_password('banana'))
+
+    def test_password_verification(self):
+      self.assertTrue(self.new_user.verify_password('banana'))
 
 class TestArticles(unittest.TestCase):
       '''
-   Test class that defines test cases for the contact class behaviours.
+   Test class that defines test cases for the article class behaviours.
    Args:
        unittest.TestCase: TestCase class that helps in creating test cases
    '''
@@ -23,7 +26,7 @@ class TestArticles(unittest.TestCase):
        '''
        Set up method to run before each test cases.
        '''
-       self.new-article = Article("1","be yourself","2","10")
+       self.new_article = Article("1","be yourself","2","10")
   
     def test_init(self):
        '''
