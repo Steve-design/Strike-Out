92920febe73af4061e013a97d76536732c1a3b99
IKERRIZ <okothfaith94@gmail.com>
2019-10-31 09:45:04 +0300

    Add  test case to test if the objects are initialize

--- tests/test_article.py
+++ tests/test_article.py
@@ -12,4 +12,20 @@ def test_no_access_password(self):
                self.new_user.password
 def test_password_verification(self):
    self.assertTrue(self.new_user.verify_password('banana'))
-   
+
+class TestArticles(unittest.TestCase):
+      '''
+   Test class that defines test cases for the contact class behaviours.
+   Args:
+       unittest.TestCase: TestCase class that helps in creating test cases
+   '''
+   def setUp(self):
+       '''
+       Set up method to run before each test cases.
+       '''
+       self.new-article = Article("1","be yourself","2","10")
+  
+    def test_init(self):
+       '''
+       test_init test case to test if the object is initialized properly
+       ''' 
